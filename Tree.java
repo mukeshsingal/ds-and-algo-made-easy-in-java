fe4e94bb71110c8102bd3d0e0a6408923ab0c5e5
mukeshsingal <mukesh.singal@kony.com>
2020-02-22 09:29:39 +0530

    Solved Basic Tree Questions

--- Tree.java
+++ Tree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 class Node {
     Node left;
     Node right;
@@ -14,18 +18,29 @@ class Node {
         right = null;
         this.value = value;
     }
+    @Override
+    public String toString() {
+        return this.value + "";
+    }
 }
 
 class Tree {
 
     public static void main(String[] args) {
         Node root = prepareTree();
-        System.out.println("\nPre ");
-        preOreder(root);
-        System.out.println("\nin");
-        inOreder(root);
-        System.out.println("\nPost ");
-        postOreder(root);
+        // System.out.println("\nPre ");
+        // preOreder(root);
+        // System.out.println("\nin");
+        // inOreder(root);
+        // System.out.println("\nPost ");
+        // postOreder(root);
+        // System.out.println("\nPost Iterative");
+        // postOrderIterative(root);
+        // System.out.println("\nLevel Order");
+        //levelOrderTraversal(root);
+        //reverseLevelOrderTraversal(root);
+        System.out.println("Height of binary tree is  " + heightOfTree(root));
+        //System.out.println("max is " + findMax(root));
     }
 
     public static Node prepareTree() {
@@ -36,6 +51,7 @@ class Tree {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
+        root.right.right.right = new Node(8);
         return root;
     }
 
@@ -63,4 +79,119 @@ class Tree {
             System.out.print(root.value + "-");
         }
     }
+
+    public static void postOrderIterative(Node root) {
+
+        if(root == null) {
+            System.out.println("Tree is empty.");
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        // We need to keep track of prev as we need to some nodes twice.
+        Node prev = null;
+
+        while(!stack.isEmpty()) {
+            Node currentNode  = stack.peek();
+
+            // Executed when we go from parent to children node
+            if(prev == null || prev.left == currentNode || prev.right == currentNode) {
+                if(currentNode.left != null) {
+                    stack.push(currentNode.left);
+                }
+                else  if(currentNode.right != null) {
+                    stack.push(currentNode.right);
+                }
+            }
+            // Executed when we go from left to parent node
+            else if(currentNode.left == prev) {
+                if(currentNode.right != null) 
+                    stack.push(currentNode.right);
+            }
+            // Executed when we go from right to parent node
+            else {
+                System.out.println(currentNode.value);
+                stack.pop();
+            }
+
+            prev = currentNode;
+        }
+
+        
+
+    }
+    public static void levelOrderTraversal(Node root) {
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            Node node = queue.remove();
+            System.out.print(node + " ");
+            if(node.left != null) {
+                queue.add(node.left);
+            }
+            if(node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public static int findMax(Node root) {
+       if(root != null) {
+           int maxLeft = findMax(root.left);
+           int maxRight = findMax(root.right);
+           return Math.max(root.value, Math.max(maxLeft, maxRight));
+       }
+       return Integer.MIN_VALUE;
+    }
+    
+    public static void reverseLevelOrderTraversal(Node root) {
+        System.out.println("\nReverse Level Order");
+        Stack<Node> stack = new Stack<>();
+
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        stack.add(root);
+
+        while(!queue.isEmpty()) {
+            Node node = queue.remove();
+        
+            
+            if(node.right != null) {
+                queue.add(node.right);
+                stack.add(node.right);
+            }
+            if(node.left != null) {
+                queue.add(node.left);
+                stack.add(node.left);
+            }
+        }
+        while(!stack.isEmpty()) {
+            Node node = stack.pop();
+            System.out.print(node + " ");
+           
+        }
+    }
+
+    public static int heightOfTree(Node root) {
+        if(root != null) {
+            int heightLeft =  heightOfTree(root.left);
+            int heightRight =  heightOfTree(root.right);
+            return Math.max(heightLeft, heightRight) + 1;
+        }
+        return 0;
+    }
+    
+
 }
+
